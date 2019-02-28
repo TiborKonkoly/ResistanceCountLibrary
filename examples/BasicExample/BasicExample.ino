6e1d8730f4334ecf004c8884a4fe650165aa7e58
TiborKonkoly <37699408+TiborKonkoly@users.noreply.github.com>
2019-02-28 18:13:54 +0100

    Add files via upload

---
+++ examples/BasicExample/BasicExample.ino
@@ -0,0 +1,49 @@
+/***************************************
+
+Resistance counting example
+
+The main aim of this example is to count the value of an unknown resistor. 
+
+Description:
+To do so we need a resistor, which's value is known. In our case the value of the known resistor is 9k67, 
+and it is hardcoded. The resistors should wired in a voltage divider circuit and the unknown resistors position should be below 
+the known one, connected to theground. To count the value of the unknown resistor we need two voltages.
+One is the output voltage of the voltage divider. This voltage is written by the function 'readVoltage()'. The secont voltage is 
+the input voltage to the voltage divider which is measured within the function 'CountResistance'. This function also provides the
+resistance of an unknown resistor in kOhm-s. 
+
+Functions:
+
+* resistance.begin() - initializes the analog pins (A4 and A5) which are used in the measurement. Use it only in the 'setup()' function.
+
+* resistance.readVoltage() - returns the output voltage from the voltage divider in V
+
+* resistance.CountResistance() - returns the value of the unknown resistor in kOhm-s 
+
+
+Created by: Tibor Konkoly
+Last update: 28.2.2019 17:20
+
+****************************************/
+
+#include "Resistance.h"
+
+
+void setup() {
+  resistance.begin();
+  Serial.begin(9600);
+}
+
+
+void loop() {
+
+float V = resistance.readVoltage();
+
+ float R = resistance.CountResistance(); 
+ 
+  Serial.print("The value of the unknown resistor is: ");
+  Serial.print(R);
+  Serial.println(" kOhm");
+  delay(1000);
+ 
+}
---
+++ examples/TheNakedTruth/TheNakedTruth.ino
@@ -0,0 +1,64 @@
+// This example contains the functions which are "working-behind-the-scene" in the library.
+
+// analog pins for reading voltages
+int Vout = 5; 
+int Vin = 4;
+
+
+// variables for reading voltages
+float voltageOut;  
+float voltageIn;
+
+// variables to hold unknown values
+float Vm;  
+float Rm;
+
+
+// variables to hold reference values
+float  Rref = 9.67; // resistance of the known resistor, measured by a multimeter
+float Vref;        // we can get this value by reading it in the CountResistance function 
+                   // reading it constantly provides an actual value, better than hardcode it 
+
+
+float k = (5.00 / 1023.00); // constant for convertint analog values to Voltage
+
+
+// functions
+
+float readVoltage(){                  // function reads the output voltage from the voltage divider
+  voltageOut = analogRead(Vout);
+  Vm = voltageOut * k;
+
+  return Vm;
+}
+
+float CountResistance(){           // function returns the value of the unknown resistor
+  voltageIn = analogRead(Vin);     // in this function we read also the Vref value
+  Vref = voltageIn * k;
+
+  Rm = (Rref * Vm) / (Vref - Vm);
+  
+  return Rm;
+}
+
+
+void setup() {
+  pinMode(Vout,INPUT);
+  pinMode(Vin,INPUT);
+  Serial.begin(9600);
+}
+
+
+void loop() {
+
+float V = readVoltage();
+
+ float R = CountResistance(); 
+ 
+  Serial.print("The value of the unknown resistor is: ");
+  Serial.print(R);
+  Serial.println(" kOhm");
+  delay(1000);
+ 
+
+}
